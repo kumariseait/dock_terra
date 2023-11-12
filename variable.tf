31bf727fae54e1b62c72d4ea779ca6df80eb09fd
kumariseait <93650590+kumariseait@users.noreply.github.com>
2023-11-12 20:05:33 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+type = string
+default = "ap-south-1"
+}
+
+variable "ami" {
+type = string
+default = "ami-0287a05f0ef0e9d9a"
+}
+
+variable "inst_type" {
+type = string
+default = "t2.micro"
+}
+
+variable "inst_az" {
+type = string
+default = "ap-south-1a"
+}
